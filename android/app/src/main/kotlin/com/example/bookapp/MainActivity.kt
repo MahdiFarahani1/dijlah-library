d8a9d6bad3972373433fe4defe1805170339a8b0
mahdi <denversmurf01@gmail.com>
2026-07-31 10:08:53 -0700

    restore local files

---
+++ android/app/src/main/kotlin/com/example/bookapp/MainActivity.kt
@@ -0,0 +1,7 @@
+package com.dijlah.library
+
+
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/core/constant/const_class.dart
@@ -0,0 +1,12 @@
+class ConstantApp {
+  static const String baseUrl = 'https://library.dijlah.org/api/v1';
+
+  static const String baseUrlGalleryTitle = '$baseUrl/upload_list/thumbs/';
+  static const String baseUrlGalleryContent = '$baseUrl/upload_list/source/';
+
+  static const String booksAPi = '$baseUrl/books';
+  static const String upload = '$baseUrl/upload_list/source/';
+  static const String downloadBook = '$baseUrl/book/download/';
+
+  static const String apiKey = 'Mdk4W8lywXXwmThwlUmSriawrdUOjvrn';
+}
